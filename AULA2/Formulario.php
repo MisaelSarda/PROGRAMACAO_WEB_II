3289bac6c9e65a4a7d9585f5627ed9ffa05f05fd
Misael <misael.sarda@unidavi.edu.br>
2025-02-26 19:46:03 -0300

    COMMIT

---
+++ AULA2/Formulario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Contato</title>
+    <link rel="stylesheet" href="formulario.css">
+</head>
+<body>
+<form action ="envia.php" method="POST"> 
+
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" required>
+
+            <label for="telefone">Telefone:</label>
+            <input type="tel" id="telefone" name="telefone" required>
+
+            <label for="email">E-mail:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="mensagem">Mensagem:</label>
+            <textarea id="mensagem" name="mensagem" required></textarea>
+
+            <button type="submit">Enviar</button>
+</form>
+</body>
+</html>
